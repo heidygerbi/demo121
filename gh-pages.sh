e2d0ee420145eb5e0d656305b7d27b8eb914a8fc
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-10-21 11:54:39 -0400

    save

---
+++ gh-pages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git branch -D gh-pages
+git push origin --delete gh-pages
+git checkout -b gh-pages
+ionic build --prod
+mv www/* .
+rm -rf www
+git add .
+git commit -m "Publishing to github pages"
+git push origin gh-pages
+git checkout master
